b0ca9a1a5b6a8165f43989790be96abb09bb18e5
Gonzalo Renes Marañón <grenes@trantor.es>
2021-12-07 23:29:13 +0100

    k8s-gitlab 5.5.2 updated

--- tpl-cskylab/k8s-gitlab/charts/gitlab/CHANGELOG.md
+++ tpl-cskylab/k8s-gitlab/charts/gitlab/CHANGELOG.md
@@ -2,6 +2,10 @@
 documentation](doc/development/changelog.md) for instructions on adding your own
 entry.
 
+## 5.5.2 (2021-12-03)
+
+No changes.
+
 ## 5.5.1 (2021-12-01)
 
 No changes.
--- tpl-cskylab/k8s-gitlab/csdeploy.sh
+++ tpl-cskylab/k8s-gitlab/csdeploy.sh
@@ -95,7 +95,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # Charts
-helm pull gitlab/gitlab --version 5.5.1 --untar
+helm pull gitlab/gitlab --version 5.5.2 --untar
 helm pull bitnami/postgresql --version 10.13.11 --untar
 
 EOF
