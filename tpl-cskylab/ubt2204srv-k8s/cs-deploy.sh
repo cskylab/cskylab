18729a6ee3917142721cf7194cda2eb0e335e2f2
Gonzalo Renes Marañón <grenes@trantor.es>
2023-07-12 21:49:30 +0200

    ubt2204srv-k8s cs-deploy.sh updated

--- tpl-cskylab/ubt2204srv-k8s/cs-deploy.sh
+++ tpl-cskylab/ubt2204srv-k8s/cs-deploy.sh
@@ -404,7 +404,8 @@ EOF
   # Install kubeadm, kubelet and kubectl
   # Ref.: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
   apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
-  curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+  curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+  # curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
   apt-get update
